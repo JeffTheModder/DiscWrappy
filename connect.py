c8a03646dd078e46d575a47d82d23cd2d30bc180
Jeff Morris <jeffreyjr.morris@gmail.com>
2021-09-01 17:05:30 -0500

    Added "on" decorator

---
+++ connect.py
@@ -0,0 +1,67 @@
+import json
+import websocket
+import threading
+import time
+import requests
+
+class Client:
+    def __init__(self):
+        self.ws = websocket.create_connection("wss://gateway.discord.gg/?v=9&encoding=json")
+
+        event = self.recieve_json_response(self.ws)
+
+        heartbeat_interval = event["d"]["heartbeat_interval"]
+        heartbeat_interval = heartbeat_interval / 1000
+
+        def heartbeat(interval, ws):
+            while True:
+                time.sleep(interval)
+                heartbeatJSON = {
+                    "op": 1,
+                    "d": "null"
+                }
+                self.send_json_request(ws, heartbeatJSON)
+
+        threading._start_new_thread(heartbeat, (heartbeat_interval, self.ws, ))
+
+    def recieve_json_response(self, ws):
+        response = ws.recv()
+        if response:
+            return json.loads(response)
+
+    def send_json_request(self, ws, request):
+        ws.send(json.dumps(request))
+    
+    def run(self, token, activity = ""):
+        self.running = True
+        payload = {
+            "op": 2,
+            "d": {
+                "token": token,
+                "intents": 513,
+                "properties": {
+                    "$os": 'windows',
+                    "$browser": 'chrome',
+                    "$device": 'pc',
+                },
+                "presence": {
+                    "activities": [{
+                        "name": activity,
+                        "type": 0
+                    }],
+                    "status": "online",
+                    "afk": False
+                }
+            }
+        }
+
+        self.send_json_request(self.ws, payload)
+        print("Logged In")
+
+    def on(self, eventType):
+        def inner(func):
+            while self.running:
+                event = self.recieve_json_response(self.ws)
+                if event["t"] == eventType:
+                    func(event)
+        return inner
---
+++ main.py
@@ -0,0 +1,9 @@
+import connect
+
+client = connect.Client()
+
+client.run("ODgyNzE4NDQ4MzczODcwNjYy.YS_dng.wbInPOY5VU5OC10pbIQFrFuzvwk", "hi")
+
+@client.on("MESSAGE_CREATE")
+def printMessageContent(message):
+    print(message["d"]["content"])
