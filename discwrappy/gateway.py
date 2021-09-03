19ffcc4f66f9a7fd13bbb2a210e69e1c2579e9a0
Jeff Morris <jeffreyjr.morris@gmail.com>
2021-09-03 16:05:23 -0500

    Fixed event listening and moved json handling to utils

--- discwrappy/gateway.py
+++ discwrappy/gateway.py
@@ -1,17 +1,18 @@
-import json
 import websocket
 import threading
 import time
 import requests
-from discwrappy.utils import DictX
+from .utils import *
+from .Objects import *
 import base64
 
 class Client:
     def __init__(self):
         self.ws = websocket.create_connection("wss://gateway.discord.gg/?v=9&encoding=json")
-        self.api_base_url = "https://discord.com/api/v9"
         
-        event = self.recieve_json_response(self.ws)
+        self.parsers = {}
+        
+        event = recieve_json_response(self.ws)
 
         heartbeat_interval = event["d"]["heartbeat_interval"]
         heartbeat_interval = heartbeat_interval / 1000
@@ -23,23 +24,16 @@ class Client:
                     "op": 1,
                     "d": "null"
                 }
-                self.send_json_request(ws, heartbeatJSON)
+                send_json_request(ws, heartbeatJSON)
 
         threading._start_new_thread(heartbeat, (heartbeat_interval, self.ws, ))
-
-    def recieve_json_response(self, ws):
-        response = ws.recv()
-        if response:
-            return json.loads(response)
-
-    def send_json_request(self, ws, request):
-        ws.send(json.dumps(request))
     
     def run(self, token, activity = ""):
-        self.running = True
         self.header = {
             "Authorization": f"Bot {token}"
         }
+        self.running = True
+
         self.bot_id = token.split(".")[0]
         self.bot_id = int(base64.b64decode(self.bot_id))
 
@@ -56,18 +50,27 @@ class Client:
             }
         }
 
-        self.send_json_request(self.ws, payload)
-        print("Logged In")
+        send_json_request(self.ws, payload)
+
+        while True:
+            event = recieve_json_response(self.ws)
+            try:
+                for i in self.parsers[event["t"]]:
+                    if event["t"] == "MESSAGE_CREATE":
+                        i(Message(event, self))
+                    else:
+                        i(event)
+            except:
+                pass
 
     def on(self, eventType):
         def inner(func):
-            while self.running:
-                event = self.recieve_json_response(self.ws)
-                event = DictX(event)
-                if event.t == eventType:
-                    if not(int(event.d["author"]["id"]) == self.bot_id):
-                        func(DictX(event.d))
+            """
+            parser idea and code by Dynamo#3349
+            """
+            try:
+                self.parsers[eventType].append(func)
+            except:
+                self.parsers[eventType] = []
+                self.parsers[eventType].append(func)
         return inner
-
-    def send(self, channel_id, msgJSON):
-        requests.post(f"{self.api_base_url}/channels/{channel_id}/messages", json=msgJSON, headers=self.header)
