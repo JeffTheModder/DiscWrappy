9e63a18c90b77a1e948aceb7ec8e31031253c7c0
Jeff Morris <jeffreyjr.morris@gmail.com>
2021-09-02 20:53:53 -0500

    Updated utils
    No longer creates an XArray or whatever it was. Just adds the period notation

--- utils.py
+++ utils.py
@@ -1,18 +1,4 @@
 class DictX(dict):
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError as k:
-            raise AttributeError(k)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __delattr__(self, key):
-        try:
-            del self[key]
-        except KeyError as k:
-            raise AttributeError(k)
-
-    def __repr__(self):
-        return '<DictX ' + dict.__repr__(self) + '>'
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
