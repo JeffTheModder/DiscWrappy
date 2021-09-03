d1c89f25c1009fcdc39a243e34045f4d2c52021a
Jeff Morris <jeffreyjr.morris@gmail.com>
2021-09-02 22:21:11 -0500

    fixed stuff

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GPL-3.0 License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
